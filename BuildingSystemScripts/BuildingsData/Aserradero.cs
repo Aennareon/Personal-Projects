e5e22f00c5c754436f566b320673db63511e9b26
Aennareon <159791743+Aennareon@users.noreply.github.com>
2024-02-12 16:24:57 +0100

    Add files via upload

---
+++ BuildingSystemScripts/BuildingsData/Aserradero.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Aserradero : buildingStats
+{
+    [Header("Estadisticas Aserradero")]
+    public float woodGenerated;
+    public bool productionON;
+}
